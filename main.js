f84f002b6ea0fba7972a38305792676a65c16a34
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-13 21:55:05 -0500

    open file example

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 var fs = require("fs");
 
-// Asynchronous read
-fs.readFile('input.txt', function (err, data) {
+// Asynchronous - Opening File
+console.log("Going to open file!");
+fs.open('input.txt', 'r+', function(err, fd) {
    if (err) {
        return console.error(err);
    }
-   console.log("Asynchronous read: " + data.toString());
+  console.log("File opened successfully!");     
 });
-
-// Synchronous read
-var data = fs.readFileSync('input.txt');
-console.log("Synchronous read: " + data.toString());
-
-console.log("Program Ended");

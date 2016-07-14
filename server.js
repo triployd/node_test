7d171d076dbf41cc580262d4026a181563d60aa7
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-14 16:50:45 -0500

    get request handling example

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+app.use(express.static('public'));
+
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.get('/process_get', function (req, res) {
+
+   // Prepare output in JSON format
+   response = {
+       first_name:req.query.first_name,
+       last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
 })
 
 var server = app.listen(8081, function () {

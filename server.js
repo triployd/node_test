9f8abedf6502e0f62983e009d2b69f0e99d4e4b9
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-14 18:04:50 -0500

    added list_users RESTful API

--- server.js
+++ server.js
@@ -2,37 +2,10 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-var bodyParser = require('body-parser');
-var multer  = require('multer');
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: '/tmp/'}));
-
-app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.htm" );
-})
-
-app.post('/file_upload', function (req, res) {
-
-   console.log(req.files.file.name);
-   console.log(req.files.file.path);
-   console.log(req.files.file.type);
-
-   var file = __dirname + "/" + req.files.file.name;
-   fs.readFile( req.files.file.path, function (err, data) {
-        fs.writeFile(file, data, function (err) {
-         if( err ){
-              console.log( err );
-         }else{
-               response = {
-                   message:'File uploaded successfully',
-                   filename:req.files.file.name
-              };
-          }
-          console.log( response );
-          res.end( JSON.stringify( response ) );
-       });
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
    });
 })
 

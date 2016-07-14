5e45011b7aedca4e5599244b194754096033ceb9
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-14 18:10:19 -0500

    add new user example

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-app.get('/listUsers', function (req, res) {
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
+app.get('/addUser', function (req, res) {
+   // First read existing users.
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data["user4"] = user["user4"];
        console.log( data );
-       res.end( data );
+       res.end( JSON.stringify(data));
    });
 })
 
@@ -13,7 +25,6 @@ var server = app.listen(8081, function () {
 
   var host = server.address().address
   var port = server.address().port
-
   console.log("Example app listening at http://%s:%s", host, port)
 
 })

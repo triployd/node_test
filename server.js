d441b84480882f07a4172a3be2b83bb65a9cb00b
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-14 17:13:06 -0500

    post request example

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('public'));
 
@@ -7,12 +11,12 @@ app.get('/index.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
 })
 
-app.get('/process_get', function (req, res) {
+app.post('/process_post', urlencodedParser, function (req, res) {
 
    // Prepare output in JSON format
    response = {
-       first_name:req.query.first_name,
-       last_name:req.query.last_name
+       first_name:req.body.first_name,
+       last_name:req.body.last_name
    };
    console.log(response);
    res.end(JSON.stringify(response));

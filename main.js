b14c41f5722619cd2f20254fb9b1bdb4c2c253c4
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-13 21:57:06 -0500

    get file stat example

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var fs = require("fs");
 
-// Asynchronous - Opening File
-console.log("Going to open file!");
-fs.open('input.txt', 'r+', function(err, fd) {
+console.log("Going to get file info!");
+fs.stat('input.txt', function (err, stats) {
    if (err) {
        return console.error(err);
    }
-  console.log("File opened successfully!");     
+   console.log(stats);
+   console.log("Got file info successfully!");
+   
+   // Check file type
+   console.log("isFile ? " + stats.isFile());
+   console.log("isDirectory ? " + stats.isDirectory());    
 });

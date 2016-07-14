1aa22b908285df3e944f4ff4ce1d871825b40ab7
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-13 22:06:53 -0500

    read directory example

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var fs = require("fs");
 
-console.log("Going to create directory /tmp/test");
-fs.mkdir('/tmp/test',function(err){
+console.log("Going to read directory /tmp");
+fs.readdir("/tmp/",function(err, files){
    if (err) {
        return console.error(err);
    }
-   console.log("Directory created successfully!");
+   files.forEach( function (file){
+       console.log( file );
+   });
 });

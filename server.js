7264c080cdc94679543e0048543f24eb17c09237
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-14 17:25:56 -0500

    express file upload example

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+var fs = require("fs");
 
-// Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var bodyParser = require('body-parser');
+var multer  = require('multer');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}));
 
 app.get('/index.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
 })
 
-app.post('/process_post', urlencodedParser, function (req, res) {
+app.post('/file_upload', function (req, res) {
 
-   // Prepare output in JSON format
-   response = {
-       first_name:req.body.first_name,
-       last_name:req.body.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
+   console.log(req.files.file.name);
+   console.log(req.files.file.path);
+   console.log(req.files.file.type);
+
+   var file = __dirname + "/" + req.files.file.name;
+   fs.readFile( req.files.file.path, function (err, data) {
+        fs.writeFile(file, data, function (err) {
+         if( err ){
+              console.log( err );
+         }else{
+               response = {
+                   message:'File uploaded successfully',
+                   filename:req.files.file.name
+              };
+          }
+          console.log( response );
+          res.end( JSON.stringify( response ) );
+       });
+   });
 })
 
 var server = app.listen(8081, function () {

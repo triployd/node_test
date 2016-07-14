a3fa4f134b9f72522a36da931fe02d1286b661bc
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-13 22:06:03 -0500

    create directory example

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
-// more file operation examples at tutorialspoint.com
 var fs = require("fs");
-var buf = new Buffer(1024);
 
-console.log("Going to open an existing file");
-fs.open('input.txt', 'r+', function(err, fd) {
+console.log("Going to create directory /tmp/test");
+fs.mkdir('/tmp/test',function(err){
    if (err) {
        return console.error(err);
    }
-   console.log("File opened successfully!");
-   console.log("Going to read the file");
-   fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
-      if (err){
-         console.log(err);
-      }
-      console.log(bytes + " bytes read");
-      
-      // Print only read bytes to avoid junk.
-      if(bytes > 0){
-         console.log(buf.slice(0, bytes).toString());
-      }
-   });
+   console.log("Directory created successfully!");
 });

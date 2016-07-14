5e79c5535d26725873356ec77dfc0bb36faf11f0
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-14 18:14:17 -0500

    delete users according to id

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-app.get('/:id', function (req, res) {
+var id = 2;
+
+app.get('/deleteUser', function (req, res) {
+
    // First read existing users.
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       users = JSON.parse( data );
-       var user = users["user" + req.params.id] 
-       console.log( user );
-       res.end( JSON.stringify(user));
+       data = JSON.parse( data );
+       delete data["user" + 2];
+       
+       console.log( data );
+       res.end( JSON.stringify(data));
    });
 })
 
